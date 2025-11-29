6c8ca5610cb2914c6da6cc7f487e97fd5a2c9fb0
r0k <padawanr0k@gmail.com>
2025-11-29 18:45:18 +0900

    fix: NumberField에서 쉼표 찍히도록 수정

--- src/common/NumberField.tsx
+++ src/common/NumberField.tsx
@@ -1,27 +1,39 @@
-import { ChangeEvent } from 'react';
-import { TextField } from 'tosslib';
+import { ChangeEvent } from "react";
+import { TextField } from "tosslib";
 
 interface NumberFieldProps {
-  value: number;
-  onChange: (value: number) => void;
-  label: string;
-  placeholder: string;
-  suffix: string;
+	value: number;
+	onChange: (value: number) => void;
+	label: string;
+	placeholder: string;
+	suffix: string;
 }
 
-export function NumberField({ value, onChange, label, placeholder, suffix }: NumberFieldProps) {
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim().replace(/[^0-9]/g, '');
-    const number = Number(value);
-    if (isNaN(number)) {
-      return;
-    }
-    onChange(number);
-  };
+export function NumberField({
+	value,
+	onChange,
+	label,
+	placeholder,
+	suffix,
+}: NumberFieldProps) {
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value.trim().replace(/[^0-9]/g, "");
+		const number = Number(value);
+		if (isNaN(number)) {
+			return;
+		}
+		onChange(number);
+	};
 
-  const numberValue = value !== undefined ? value.toString() : '';
+	const numberValue = value !== undefined ? value.toLocaleString() : "";
 
-  return (
-    <TextField onChange={handleChange} value={numberValue} label={label} placeholder={placeholder} suffix={suffix} />
-  );
+	return (
+		<TextField
+			onChange={handleChange}
+			value={numberValue}
+			label={label}
+			placeholder={placeholder}
+			suffix={suffix}
+		/>
+	);
 }
